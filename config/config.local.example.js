04487b3d43a8eb3cba428d674c894d0de69a61d7
case <7795958+lipangpang251@user.noreply.gitee.com>
2022-09-07 10:28:11 +0800

    default database name

--- config/config.local.example.js
+++ config/config.local.example.js
@@ -28,7 +28,7 @@ module.exports = appInfo => {
           port: 3306,
           user: 'root',
           password: '123456',
-          database: 'jianghujs-finance'
+          database: 'jianghujs_finance'
         },
         pool: { min: 0, max: 10 },
         acquireConnectionTimeout: 30000
