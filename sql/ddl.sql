86951b945dad58d44ee66b0d15e720d2c1154b5a
Liu Ji <mynameisliuji@foxmail.com>
2022-11-25 15:16:52 +0800

    requirment.md

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,3 +1,23 @@
+-- period: 会计区间表
+-- subject: 科目表; 科目分类、余额方向
+    -- subject_balance: 科目-期余额
+-- voucher: 凭证表; 
+    -- voucher_entry: 凭证条目====》科目ID、借方金额、贷方金额
+-- profit: 资产负债配置
+    -- profit_formula: 资产负债计算规则
+
+DROP TABLE IF EXISTS `period`;
+CREATE TABLE `period` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `periodId` varchar(255) DEFAULT NULL COMMENT '会计区间Id; 月份维度 E.g: 2023-03',
+  `is_checkout` varchar(255) DEFAULT NULL COMMENT '科目名称;',
+  `subjectCategory` varchar(255) DEFAULT NULL COMMENT '科目分类;资产, 负债, 权益, 成本, 损益',
+  `parentSubjectId` varchar(255) DEFAULT NULL COMMENT '父级科目id;',
+  `balanceDirection` varchar(255) DEFAULT NULL COMMENT '余额方向;借, 贷',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB COMMENT = '会计区间表';
+
+DROP TABLE IF EXISTS `subject`;
 CREATE TABLE `subject` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `subjectId` varchar(255) DEFAULT NULL COMMENT '科目编码;',
@@ -8,7 +28,7 @@ CREATE TABLE `subject` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB COMMENT = '科目表';
 
-
+DROP TABLE IF EXISTS `voucher`;
 CREATE TABLE `voucher` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `voucherId` varchar(255) DEFAULT NULL COMMENT '凭证id;',
--- sql/requirment.md
+++ sql/requirment.md
@@ -3,28 +3,25 @@
 ## 页面
 
 - 基础设置
-    - 科目管理 页面
+    - 会计期间: period; 新增会计区间时 创建 对应的 基础数据 ===》subject_balance
+    - 科目管理 页面: subject subject_balance
+    > subject_balance 初始化/定时任务生成
 - 记账
-    - 录凭证 页面
-    - 凭证列表 页面
+    - 录凭证 页面: voucher voucher_entry subject_balance
+    - 凭证列表 页面: voucher voucher_entry subject_balance
 - 查账
-    - 账目明细 页面: 科目维度 
-    - 总账 页面: 期初余额-本期合计-本年累计
+    - 账目明细 页面: 科目维度  voucher voucher_entry subject_balance
+    - 总账 页面: 期初余额-本期合计-本年累计  voucher voucher_entry subject_balance
 - 报表
-    - 资产负债表: 通过公式配置 将 凭证算出 资产负债
+    - 资产负债表: 通过公式配置 将 科目余额 算出 资产负债: 
+        - profit profit_formula subject_balance
 
 ## 表
 
-- subject: 科目表; 
+- period: 会计区间
+- subject: 科目表; 科目分类、余额方向
+    - subject_balance: 科目-期余额
 - voucher: 凭证表; 
     - voucher_entry: 凭证条目====》科目ID、借方金额、贷方金额
--     
-
-
-## 财务的 核心是
-
-- 记账：不同科目的计帐
-    - 科目
-    - 
-- 查账/对账:  审计部门, 汇总/统计 帐目信息
-- 付款 & 合同
+- profit: 资产负债配置
+    - profit_formula: 资产负债计算规则
