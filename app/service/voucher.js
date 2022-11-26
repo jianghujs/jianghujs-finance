9da0d07cbc1d063213274a27f9b572c8bac29110
zhongchubing <861013016@qq.com>
2022-11-26 15:26:22 +0800

    feat: 凭证和凭证条目功能

--- app/service/voucher.js
+++ app/service/voucher.js
@@ -1,34 +1,35 @@
 const Service = require('egg').Service;
 const idGenerateUtil = require("@jianghujs/jianghu/app/common/idGenerateUtil");
 const validateUtil = require('@jianghujs/jianghu/app/common/validateUtil');
-const { subjectCategoryEnum } = require('../constant/constant');
+const { subjectCategoryEnum, tableEnum } = require('../constant/constant');
 
 const actionDataScheme = Object.freeze({
-  fillInsertItemParamsBeforeHook: {
+  createVoucherAndVoucherEntry: {
     type: 'object',
     additionalProperties: true,
-    required: [ 'subjectCategory'],
+    required: ['voucherEntryList', 'periodId', 'voucherCreateAt'],
     properties: {
-      subjectCategory: { type: 'string' },
+      voucherEntryList: { type: 'array' },
+      periodId: { type: 'string' },
+      voucherCreateAt: { type: 'string' },
     },
   },
 });
 
 class VoucherService extends Service {
-  async fillInsertItemParamsBeforeHook() {
-    const { jianghuKnex } = this.app
-    const actionData = this.ctx.request.body.appData.actionData;
-    const { userId, username } = this.ctx.userInfo;
+
+  async _buildVoucherParam() {
+    const { jianghuKnex } = this.app;
+    const { username } = this.ctx.userInfo;
 
     const voucherNumber = await idGenerateUtil.idPlus({
       knex: jianghuKnex,
-      tableName: 'voucher',
+      tableName: tableEnum.voucher,
       columnName: 'voucherNumber',
     });
-    const voucherName = '记2022'
+    const voucherName = '记2022';
 
-    this.ctx.request.body.appData.actionData = {
-      ...actionData,
+    return {
       voucherName,
       voucherNumber,
       voucherId: `${voucherName}${voucherNumber}`,
@@ -38,7 +39,39 @@ class VoucherService extends Service {
 
   // 创建凭证和凭证条目
   async createVoucherAndVoucherEntry() {
-    
+    const { jianghuKnex } = this.app
+    const { actionData } = this.ctx.request.body.appData
+    validateUtil.validate(actionDataScheme.createVoucherAndVoucherEntry, actionData, '凭证生成');
+    const { voucherEntryList, ...data } = actionData;
+
+    const voucherInfo = await this._buildVoucherParam();
+
+    const { voucherId } = voucherInfo;
+    const voucherEntryPromise = [];
+    voucherEntryList.forEach(item => {
+      voucherEntryPromise.push(
+        jianghuKnex(tableEnum.voucher_entry).insert({ ...item, voucherId })
+      )
+      // 创建科目余额
+      voucherEntryPromise.push(
+        jianghuKnex(tableEnum.subject_balance).insert({
+          periodId: data.periodId,
+          subjectId: item.subjectId,
+          debit: item.debit,
+          credit: item.credit,
+        })
+      )
+    });
+    // TODO 借贷不平衡判断
+
+    await Promise.all([
+      // 创建凭证
+      jianghuKnex(tableEnum.voucher).insert({
+        ...data,
+        ...voucherInfo
+      }),
+      ...voucherEntryPromise
+    ]);
   }
 }
 
