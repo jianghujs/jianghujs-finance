d4cafa550c7220e802a93c558805ae7b777038ee
Liu Ji <mynameisliuji@foxmail.com>
2022-12-13 14:36:29 +0800

    jianghujs_finance

--- config/config.local.example.js
+++ config/config.local.example.js
@@ -28,7 +28,7 @@ module.exports = appInfo => {
           port: 3306,
           user: 'root',
           password: '123456',
-          database: 'jianghujs_finance'
+          database: 'fs_finance'
         },
         pool: { min: 0, max: 10 },
         acquireConnectionTimeout: 30000
